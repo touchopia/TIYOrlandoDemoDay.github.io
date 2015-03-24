42dd9728db60b5b5441e8fec57681963d7edaa93
LoganArnett <Loganarn5@gmail.com>
2015-03-24 17:01:51 -0400

    updated frames and ready for info

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,64 +1,29 @@
 'use strict';
 
 angular.module('tiyorlandoDemoDaygithubio')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+  .controller('MainCtrl', function () {
+    this.frontEnders = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        'title': 'Whats Pup',
+        'url': 'github.com/WhatsPup',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'students': {
+            'name1': 'Carolina Powers',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'name2': 'Jared Stevick',
+            'img2': 'http://lorempixel.com/200/200/sports'
+        }
       },
       {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      },
-      {
-        'title': 'Angular UI Bootstrap',
-        'url': 'http://angular-ui.github.io/bootstrap/',
-        'description': 'Bootstrap components written in pure AngularJS by the AngularUI Team.',
-        'logo': 'ui-bootstrap.png'
-      },
-      {
-        'title': 'Sass (Node)',
-        'url': 'https://github.com/sass/node-sass',
-        'description': 'Node.js binding to libsass, the C version of the popular stylesheet preprocessor, Sass.',
-        'logo': 'node-sass.png'
+        'title': 'Ready Read',
+        'url': 'github.com/ReadyRead',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'students': {
+            'name1': 'John Alexander',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'name2': 'Taylor Foster',
+            'img2': 'http://lorempixel.com/200/200/people'
+        }
       }
     ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
   });
