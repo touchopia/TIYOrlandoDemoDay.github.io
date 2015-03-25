52226c6ea20c62369a7ff7b79c7e831261f4d060
LoganArnett <Loganarn5@gmail.com>
2015-03-25 16:28:41 -0400

    adding in student info

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,47 +7,147 @@ angular.module('tiyorlandoDemoDaygithubio')
         'title': 'Whats Pup',
         'url': 'github.com/WhatsPup',
         'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': '',
         'students': {
             'name1': 'Carolina Powers',
             'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'carolinapowers',
+            'twit1': 'carolinapowers',
+            'email1': 'carolinapowers@gmail.com',
             'name2': 'Jared Stevick',
-            'img2': 'http://lorempixel.com/200/200/sports'
+            'img2': 'http://lorempixel.com/200/200/sports',
+            'git2': 'jsteves',
+            'twit2': 'jsteves',
+            'email2': 'jsteves',
+        }
+      },
+       {
+        'title': 'Study Buddy',
+        'url': 'https://github.com/eklemen/study-buddy',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/eklemen/study-buddy',
+        'students': {
+            'name1': 'Evan Klemen',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'eklemen',
+            'twit1': 'evanklemen',
+            'email1': 'ejklemen@gmail.com',
+            'name2': false,
+            
         }
       },
       {
-        'title': 'Ready Read',
-        'url': 'github.com/ReadyRead',
+        'title': 'Skedaddle',
+        'url': 'skedaddle.click',
         'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/sak55/skedaddle',
+        'students': {
+            'name1': 'Sam Kauffman',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'sak55',
+            'twit1': 'samkauffman',
+            'email1': 'samuelkauffman@gmail.com',
+            'name2': false,
+        }
+      },
+      {
+        'title': 'ReadyRead',
+        'url': 'github.com/WhatsPup',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': '',
         'students': {
             'name1': 'John Alexander',
             'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'carolinapowers',
+            'twit1': 'carolinapowers',
+            'email1': 'carolinapowers@gmail.com',
             'name2': 'Taylor Foster',
-            'img2': 'http://lorempixel.com/200/200/people'
+            'img2': 'http://lorempixel.com/200/200/sports',
+            'git2': 'jsteves',
+            'twit2': 'jsteves',
+            'email2': 'jsteves',
         }
-      }
+      },
+      {
+        'title': 'MadEase',
+        'url': 'github.com/WhatsPup',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': '',
+        'students': {
+            'name1': 'Maddie Fees',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'carolinapowers',
+            'twit1': 'carolinapowers',
+            'email1': 'carolinapowers@gmail.com',
+            'name2': false,
+            
+        }
+      },
+        {
+        'title': 'Fowl Landing',
+        'url': 'https://github.com/adamkarb/Fowl-Landing',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/adamkarb/Fowl-Landing',
+        'students': {
+            'name1': 'Adam Karbiener',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'adamkarb',
+            'twit1': 'adamkarb',
+            'email1': 'adamkarb@gmail.com',
+            'name2': false,
+        }
+      },
+      {
+        'title': 'HausBrewer',
+        'url': 'hausbrewer.com',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/jrutishauser/hausbrewer',
+        'students': {
+            'name1': 'Jon Rutishauser',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'git1': 'jrutishauser',
+            'twit1': 'jonrutis',
+            'email1': 'jrutishauser@gmail.com',
+            'name2': false,
+        }
+      },
     ];
     this.rubyists = [
       {
-        'title': 'Whats Pup',
-        'url': 'github.com/WhatsPup',
+        'title': 'New Start',
+        'url': 'https://github.com/new-start/project',
         'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/new-start/project',
         'students': {
-            'name1': 'Carolina Powers',
+            'name1': 'J. Spencer Hopkins',
             'img1': 'http://lorempixel.com/200/200/sports',
-            'name2': false,
-            'img2': ''
+            'git1': 'jspencerhopkins',
+            'twit1': 'jspencerhopkins',
+            'email1': 'jspencerhopkins@gmail.com',
+            'name2': 'Peter Ortiz',
+            'img2': 'http://lorempixel.com/200/200/sports',
+            'git2': 'jsteves',
+            'twit2': 'jsteves',
+            'email2': 'jsteves',
         }
       },
       {
-        'title': 'Ready Read',
-        'url': 'github.com/ReadyRead',
+        'title': 'Spin Off Newsletter',
+        'url': 'https://github.com/new-start/project',
         'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'repo': 'https://github.com/new-start/project',
         'students': {
-            'name1': 'John Alexander',
+            'name1': 'Andell Jean-Jacques',
             'img1': 'http://lorempixel.com/200/200/sports',
-            'name2': 'Taylor Foster',
-            'img2': 'http://lorempixel.com/200/200/people'
+            'git1': 'ajean144',
+            'twit1': 'andell_a',
+            'email1': 'ajeanjacques42@gmail.com',
+            'name2': 'Jeff Winkler',
+            'img2': 'http://lorempixel.com/200/200/sports',
+            'git2': 'jsteves',
+            'twit2': 'jsteves',
+            'email2': 'jsteves',
         }
-      }
+      },
     ];
   });
