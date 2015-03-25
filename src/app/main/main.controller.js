32f0c048d362a26e97398ee643fb9cefe20c3d22
LoganArnett <Loganarn5@gmail.com>
2015-03-25 12:17:15 -0400

    finished opposite images

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -26,4 +26,28 @@ angular.module('tiyorlandoDemoDaygithubio')
         }
       }
     ];
+    this.rubyists = [
+      {
+        'title': 'Whats Pup',
+        'url': 'github.com/WhatsPup',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'students': {
+            'name1': 'Carolina Powers',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'name2': false,
+            'img2': ''
+        }
+      },
+      {
+        'title': 'Ready Read',
+        'url': 'github.com/ReadyRead',
+        'description': 'CRM for Pet Sitters, A Happy Pup makes a Happy Owner makes a Happy Sitter',
+        'students': {
+            'name1': 'John Alexander',
+            'img1': 'http://lorempixel.com/200/200/sports',
+            'name2': 'Taylor Foster',
+            'img2': 'http://lorempixel.com/200/200/people'
+        }
+      }
+    ];
   });
